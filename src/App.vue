431eb53f0116861092e9ff267066cb883301ef49
Moreno Kevin <56068021+MorenoKevin@users.noreply.github.com>
2019-12-11 11:23:28 +0100

    Implémentation de ListTitle et modif. de App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="col-m-4">
+      <h2>{{ title }}</h2> <!-- {{ double brackets make it possible to use the vue variables directly in html,
+                                    here you use the title coming from the data section of this component (App.vue) -->
+      <div class="footer">
+        <hr />
+        <!-- v-on directive that will process a custom event called onEnter (emitted by child component), if this event
+        occurs, the parent will call the method changeTitle -->
+        <list-title v-on:onEnter="changeTitle"></list-title> <!--use a specific tag which is your component name in kebab-case (list-title) -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/*import the file containing the component definition */
+import ListTitle from "./components/ListTitle.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    /* "Declare" the component in the js of the parent*/
+    ListTitle
+  },
+  data() {
+    return {
+      title: 'MyVueJS Todo List' /* variable containing the title of the page */
+    };
+  },
+  methods: {
+    /* the method changeTitle, will chaange the title of the page when the onEnter event occurs */
+    changeTitle(text){
+      this.title = text;
+    }
+  },
+};
 </script>
 
 <style>
--- src/components/ListTitle.vue
+++ src/components/ListTitle.vue
@@ -1,11 +1,34 @@
 <template>
-
+<div>
+    <div>
+        <em>Set a title and press Enter</em> <!--This is the text that appears above the input -->
+        <!--v-bind is a vue directive which links the javascript variables to the html
+        for the input below, the value is coming from a js variable called title-->
+        <input v-bind:value="title" v-on: @keyup.enter="onEnter"/>
+        <!-- v-on is also a vue directive, that allows to set a behavior on a specific event-->
+    </div>
+</div>
 </template>
 
 <script>
-
+export default {
+    /*When children components want to have access to the parent's data,
+    they must declare "props" on the component initialization
+    The list title is set in this component, but the parent App.vue has to show the list title, this is why it's a prop
+    */
+    props: ['title'],
+    methods: {
+        /*this method is called when the Enter key is pressed inside the input (see v-on: on the input up)*/
+        onEnter (event) {
+            /* since the value of the list title is in the App.vue component (parent), we need to inform it that we want to change the title
+            this is why we need to "send" the event to the parent with the $emit function */
+            this.$emit('onEnter',event.target.value); /*param1 : event name, param2 : the value sent to the parent (here input value=new title)*/
+            this.title = ""; /*reset the input after emitting the event */
+        }
+    }
+}
 </script>
 
-<style>
+<style scoped>
 
 </style>
